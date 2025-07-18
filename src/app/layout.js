65a351e61322302a32724f18691151176a3b0e8c
Vis-star-sys <vishal12suthar@gmail.com>
2025-07-18 11:50:11 +0530

    adm

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,31 +21,61 @@ export default function RootLayout({ children }) {
           content="A portfolio showcasing my work as a frontend developer"
         />
         <meta name="author" content="Vishal Suthar" />
-        <meta name="keywords" content="portfolio, frontend developer, vishal suthar" />
+        <meta
+          name="keywords"
+          content="portfolio, frontend developer, vishal suthar"
+        />
         <meta name="robots" content="index, follow" />
-        <meta name="google-site-verification" content="your-verification-code" />
+        <meta
+          name="google-site-verification"
+          content="your-verification-code"
+        />
         <meta property="og:title" content="Portfolio - Vishal Suthar" />
-        <meta property="og:description" content="A portfolio showcasing my work as a frontend developer" />
+        <meta
+          property="og:description"
+          content="A portfolio showcasing my work as a frontend developer"
+        />
         <meta property="og:image" content="/og-image.png" />
-        <meta property="og:url" content="https://vishalsuthar.com"  />
+        <meta property="og:url" content="https://vishalsuthar.com" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Portfolio - Vishal Suthar" />
-        <meta name="twitter:description" content="A portfolio showcasing my work as a frontend  developer" />
+        <meta
+          name="twitter:description"
+          content="A portfolio showcasing my work as a frontend  developer"
+        />
         <meta name="twitter:image" content="/og-image.png" />
         <meta name="twitter:site" content="@vishalsuthar" />
         <meta name="twitter:creator" content="@vishalsuthar" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="msapplication-TileImage" content="/mstile-150x150.png" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
-       
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3083695600321028"
+          crossorigin="anonymous"
+        ></script>
       </head>
 
       <body className="cursor-none">
